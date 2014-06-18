c8100b75f0a03acfe4fe2181dfb2aae2ca4f4077
Ka-Ping Yee <ping@zesty.ca>
2014-06-18 21:36:15 +0100

    Tolerate extra text appended to report data; clarify error feedback.

--- controllers/commands/report.js
+++ controllers/commands/report.js
@@ -8,13 +8,14 @@ var util = require('util'),
 
 var MESSAGES = {
     noSurveyFound: 'No survey found for this phone number.',
-    registerFirst: 'This phone number has not been registered. Please use a registered phone or call the hotline for help.',
-    questions: '[MSF]: Please enter data for %s, %s:',
+    notRegistered: 'Your phone number has not been registered. Please use a registered phone or call the hotline for help.',
+    survey: 'Please enter data for %s, %s in this order: %s. For any unknown items, enter "U".',
     chooseLocation: 'What location would you like to report for? Text the number in front of the name to choose:\n',
-    numericInputRequired: 'Error: numeric input required for %s.',
+    commaSeparatedFieldsRequired: 'Please enter data for %s, %s as %d items with a comma after each: %s. For any unknown items, enter "U".',
+    numberRequired: 'A number (or "U" for unknown) is required for %s. Please enter data for %s, %s in this order: %s.',
     locationNumberError: 'Error: Please choose a valid number from these choices:\n',
     anyOtherDiseases: 'Any other diseases to report? Please provide details.',
-    confirmReport: 'Please review your report for %s, %s:\n%s.\nIs this correct? Text "yes" or "no".',
+    confirmReport: 'Please review your report for %s, %s:\n%s.\nIs this correct? Reply "yes" or "no".',
     generalError: 'Sorry, there was a problem with the system. Please try again.',
     thanks: 'Your report has been submitted. Thank you!',
 };
@@ -26,10 +27,11 @@ var CRISIS_MAP_API_URL = 'https://msfcrisismap.appspot.com/.api/reports';
 function printResponses(questions, responses) {
     var answers = [];
     for (var i = 0; i < responses.length; i++) {
-        var q = questions[i], r = responses[i];
-        var tr = r.textResponse;
-        if (q.responseType === 'number' && r.numberResponse === null) {
-            tr = 'Unknown';
+        var q = questions[i], r = responses[i], tr;
+        if (q.responseType === 'number') {
+            tr = (r.numberResponse === null) ? 'Unknown' : r.numberResponse;
+        } else {
+            tr = r.textResponse;
         }
         answers.push(q.summaryText + ': ' + tr);
     }
@@ -48,6 +50,8 @@ function printLocations(placeIds) {
 
 // Submit a survey response to Crisis Map using its API.
 function pushToCrisisMap(survey, response, reporter) {
+    var location = locations.all[response.placeId];
+
     // data needed to submit a reply to the Crisis Map API
     var formData = {
         source: SOURCE_URL,
@@ -57,8 +61,8 @@ function pushToCrisisMap(survey, response, reporter) {
         topic_ids: [survey.cmId + '.' + survey.cmTopicId],
         submitted: response.completedOn.getTime()/1000,
         effective: new Date().getTime()/1000, // TODO: EPI week
-        location: [reporter.locationLat, reporter.locationLng], // TODO
-        place_id: reporter.placeId, // TODO
+        location: [location.centroidLat, location.centroidLng],
+        place_id: response.placeId,
         answers: {}
     };
 
@@ -108,22 +112,35 @@ function pushToCrisisMap(survey, response, reporter) {
 module.exports = function(number, step, message, survey, reporter, callback) {
     // Defaults to current epi week, need to make this configurable
     var interval = epi(moment().tz('Africa/Lagos').toDate());
+    var intervalName = 'Week ' + interval.week;
 
     if (!survey) {
         callback(null, MESSAGES.noSurveyFound);
         return;
     }
     if (!reporter.placeIds || !reporter.placeIds.length) {
-        callback(null, MESSAGES.registerFirst);
+        callback(null, MESSAGES.notRegistered);
         return;
     };
+
+    var dataList = survey.questions.map(function(q) { return q.summaryText; });
+    var questionList = dataList.join(',\n');
+    function getPlaceName() {
+        return locations.all[reporter.currentPlaceId].name;
+    }
+    function printSurvey() {
+        return util.format(
+            MESSAGES.survey, getPlaceName(), intervalName, questionList);
+    }
 
     if (step === 0) {
         // determine the current place ID
         if (reporter.placeIds.length === 1) {
             // If the user only has one, set it and move on
             reporter.currentPlaceId = reporter.placeIds[0];
-            step = 2;
+            callback(null, printSurvey(), 2);
+            return;
+            // fall through to step 2
         } else {
             // Ask the user to choose their location
             callback(null, 
@@ -141,58 +158,82 @@ module.exports = function(number, step, message, survey, reporter, callback) {
                 printLocations(reporter.placeIds);
             callback(null, msg, 1);
             return;
-        } else {
-            reporter.currentPlaceId = reporter.placeIds[placeIndex-1];
-            callback(null, printSurvey(), 2);
-            return;
         }
+        reporter.currentPlaceId = reporter.placeIds[placeIndex-1];
+        callback(null, printSurvey(), 2);
+        return;
     }
 
     if (step === 2) {
         // Receive comma-separated data, ask for confirmation.
         var answerInputs = message.split(',');
-        if (answerInputs.length === survey.questions.length) {
-            // try to use these answers for the actual report
-            var responses = [];
-            for (var i = 0; i<answerInputs.length; i++) {
-                var answerText = answerInputs[i].trim(),
-                    question = survey.questions[i];
-
-                if (question.responseType === 'number') {
-                    var casted = Number(answerText);
-                    if (answerText.toUpperCase() === 'U') {
-                        // let users enter "u" to mean "unknown"
-                        casted = null;
-                    }
-                    if (!isNaN(casted)) {
-                        responses.push({
-                            _questionId: question._id,
-                            textResponse: answerText,
-                            numberResponse: casted  // a number or null
-                        });
-                    } else {
-                        callback(null, util.format(
-                            MESSAGES.numericInputRequired,
-                            question.summaryText
-                        ) + ' ' + printSurvey(), 2);
-                        return;
-                    }
-                } else {
-                    // for now throw everything else in as just text
-                    responses.push({
-                        _questionId: question._id,
-                        textResponse: answerText
-                    });
-                }
-            }
-
-            // Now that we have answers processed, create a survey response
-            updateSurveyResponse(responses);
-
-        } else {
-            // otherwise, print out current questions
-            callback(null, printSurvey(), 2);
+        if (answerInputs.length < survey.questions.length) {
+            callback(null, util.format(MESSAGES.commaSeparatedFieldsRequired,
+                getPlaceName(), intervalName, survey.questions.length,
+                questionList), 2);
+            return;
         }
+
+        var responses = [];
+        for (var i = 0; i < survey.questions.length; i++) {
+            var answerText = answerInputs[i].trim();
+            var question = survey.questions[i];
+            if (question.responseType === 'number') {
+                var firstWord = answerText.split(/\s+/)[0];
+                var numericValue = Number(firstWord);
+                if (firstWord.toUpperCase() === 'U') {
+                    // Let users enter "u" to mean "unknown".
+                    numericValue = null;
+                }
+                if (isNaN(numericValue)) {
+                    callback(null, util.format(MESSAGES.numberRequired,
+                        question.summaryText, getPlaceName(), intervalName,
+                        questionList), 2);
+                    return;
+                }
+                responses.push({
+                    _questionId: question._id,
+                    textResponse: answerText,
+                    numberResponse: numericValue
+                });
+            } else {
+                // For now throw everything else in as text.
+                responses.push({
+                    _questionId: question._id,
+                    textResponse: answerText
+                });
+            }
+        }
+
+        // Save the current data to a new or existing SurveyResponse.
+        SurveyResponse.findOne({
+            _surveyId: survey._id,
+            _reporterId: reporter._id,
+            placeId: reporter.currentPlaceId,
+            interval: interval
+        }, function(err, sr) {
+            sr = sr || new SurveyResponse({
+                _surveyId: survey._id,
+                _reporterId: reporter._id,
+                placeId: reporter.currentPlaceId,
+                interval: interval
+            });
+            sr.phoneNumber = number;
+            sr.complete = false;
+            sr.commentText = '';
+            sr.responses = responses;
+            sr.save(function(err) {
+                if (err) {
+                    console.log(err);
+                    callback(err, MESSAGES.generalError);
+                    return;
+                }
+                callback(null, util.format(
+                    MESSAGES.confirmReport, getPlaceName(), intervalName,
+                    printResponses(survey.questions, sr.responses)
+                ), 3);
+            });
+        });
         return;
     }
 
@@ -201,27 +242,28 @@ module.exports = function(number, step, message, survey, reporter, callback) {
         var words = message.replace(/[^\w\s]/g, '').trim().split(/\s+/);
         if (words[0].toLowerCase() === 'yes') {
             callback(null, MESSAGES.anyOtherDiseases, 4);
-        } else if (words[0].toLowerCase() === 'no') {
-            callback(null, printSurvey(), 2);
-        } else {
-            SurveyResponse.findOne({
-                _surveyId: survey._id,
-                _reporterId: reporter._id,
-                placeId: reporter.currentPlaceId,
-                interval: interval
-            }, function(err, sr) {
-                if (err || !sr) {
-                    callback(err || 'missing sr', printSurvey(), 2);
-                    return;
-                }
-                callback(null, util.format(
-                    MESSAGES.confirmReport,
-                    locations.all[reporter.currentPlaceId].name,
-                    'Week ' + interval.week,
-                    printResponses(survey.questions, sr.responses)
-                ), 3);
-            });
+            return;
         }
+        if (words[0].toLowerCase() === 'no') {
+            callback(null, printSurvey(), 2);
+            return;
+        }
+        SurveyResponse.findOne({
+            _surveyId: survey._id,
+            _reporterId: reporter._id,
+            placeId: reporter.currentPlaceId,
+            interval: interval
+        }, function(err, sr) {
+            if (err || !sr) {
+                callback(err || 'missing sr', printSurvey(), 2);
+                return;
+            }
+            callback(null, util.format(
+                MESSAGES.confirmReport, getPlaceName(), intervalName,
+                printResponses(survey.questions, sr.responses)
+            ), 3);
+        });
+        return;
     }
 
     if (step === 4) {
@@ -243,54 +285,6 @@ module.exports = function(number, step, message, survey, reporter, callback) {
                 callback(err, MESSAGES.thanks);
                 if (survey.cmId) {
                     pushToCrisisMap(survey, sr, reporter);
-                }
-            });
-        });
-    }
-
-    // print out survey questions
-    function printSurvey() {
-        var dataList = survey.questions.map(function(question) {
-            return question.summaryText;
-        });
-        var baseMessage = util.format(
-            MESSAGES.questions,
-            locations.all[reporter.currentPlaceId].name,
-            'Week ' + interval.week
-        );
-        return baseMessage + '\n' + dataList.join(',\n');
-    }
-
-    // Save the current data to a new or existing SurveyResponse.
-    function updateSurveyResponse(responses) {
-        // Create new or update pending response
-        SurveyResponse.findOne({
-            _surveyId: survey._id,
-            _reporterId: reporter._id,
-            placeId: reporter.currentPlaceId,
-            interval: interval
-        }, function(err, sr) {
-            sr = sr || new SurveyResponse({
-                _surveyId: survey._id,
-                _reporterId: reporter._id,
-                placeId: reporter.currentPlaceId,
-                interval: interval
-            });
-            sr.phoneNumber = number;
-            sr.complete = false;
-            sr.commentText = '';
-            sr.responses = responses;
-            sr.save(function(err) {
-                if (err) {
-                    console.log(err);
-                    callback(err, MESSAGES.generalError);
-                } else {
-                    callback(null, util.format(
-                        MESSAGES.confirmReport,
-                        locations.all[reporter.currentPlaceId].name,
-                        'Week ' + interval.week,
-                        printResponses(survey.questions, sr.responses)
-                    ), 3);
                 }
             });
         });
